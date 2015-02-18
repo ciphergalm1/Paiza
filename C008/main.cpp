49e5d629460109835d480a715b53b0d57cc9b4be
ciphergalm1 <adfxciphergalm1@outlook.com>
2015-02-18 11:27:00 +0900

    update1

--- C008/main.cpp
+++ C008/main.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 int main(){
+	string str1, str2;
+	string target;
+	cin >> str1 >> str2;
+	cin >> target;
+
+	int length1 = str1.size();
+	int length2 = str2.size();
+
+	int pos = 0;
+	while (pos < target.size()){
+		target.find(str1,pos);
+
+	}
+
+	//cout << str1 << str2<<endl;
 
 
+	system("pause");
 	return 0;
 }
